2fd575011bb0fc4cf485465ed590a19b056fe825
MukundThorat <mukundthorat08@gmail.com>
2024-02-08 13:54:05 +0530

    fix bugs

--- SourceCode/Js/content.js
+++ SourceCode/Js/content.js
@@ -3,19 +3,20 @@
 // Description : Automation task
 
 function check_display_value(wait_btn, button_to_click, after_click){
-    if (wait_btn != null){
+    let interval;
+    if (wait_btn != null) {
         let isDisplay_none = window.getComputedStyle(wait_btn).getPropertyValue('display')
 
-        if (isDisplay_none == "none"){
-            
-            chrome.runtime.sendMessage({ display_log: true });
+        if (isDisplay_none === "none") {
+
+            chrome.runtime.sendMessage({display_log: true});
             localStorage.setItem("current_page", window.location.href)
             button_to_click.click()
             clearInterval(interval)
-            if (after_click == true){
+            if (after_click === true) {
                 interval = setInterval(check_display_value, 1000, document.getElementById("lite-end-sora-wait"), document.getElementById("lite-end-sora-button"))
 
-                if (window.location.href != localStorage.getItem("current_page")){
+                if (window.location.href !== localStorage.getItem("current_page")) {
                     console.log("error with accessing new page")
                 }
 
@@ -26,20 +27,29 @@ function check_display_value(wait_btn, button_to_click, after_click){
 }
 
 interval = setInterval(check_display_value, 1000, document.getElementById("lite-human-verif-wait"), document.getElementById("lite-human-verif-button"))
-
-if (window.location.href != localStorage.getItem("current_page")){
+console.log(window.href)
+if (window.location.href !== localStorage.getItem("current_page")){
     interval = setInterval(check_display_value, 1000, document.getElementById("lite-start-sora-wait"), document.getElementById("lite-start-sora-button"), true)
     log_message_list = JSON.parse(localStorage.getItem('msg_list'))
 }
-if (window.location.href != localStorage.getItem("current_page")){
-    console.log("gdlfix")
-    if(document.getElementById("drc")!=null){
+if (window.location.href !== localStorage.getItem("current_page")){
+    if (document.getElementById("btn-secondary")!=null){
+        document.getElementById("btn-secondary").click();
+    }else if (document.getElementById("btn-danger")!=null){
+        document.getElementById("btn-danger").click();
+    }else if(document.getElementById("drc")!=null){
         document.getElementById("drc").click();
-
     }else if(document.querySelector("#cf_captcha > div.card-body > div:nth-child(2) > a.btn.btn-success")!=null){
         document.querySelector("#cf_captcha > div.card-body > div:nth-child(2) > a.btn.btn-success").click()
     }
-    if(document.getElementById("dev_button")!=null){
+
+    if(document.getElementById("dev_button")!=null) {
         document.getElementById("dev_button").click()
+    }
+    if(document.getElementById("ins")!=null) {
+        document.getElementById("ins").click()
+    }
+    if(document.getElementById("btn-success")!=null) {
+        document.getElementById("btn-success").click()
     }
 }
--- SourceCode/Js/popup.js
+++ SourceCode/Js/popup.js
@@ -5,13 +5,14 @@ console.log(JSON.parse(localStorage.getItem("msg_list")))
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log("received")
-    if (request.display_log == true){
-        log_message_list.push("succed with clicking on button...")
-        localStorage.setItem('msg_list',  JSON.stringify(log_message_list));
+    let message_lst;
+    if (request.display_log === true) {
+        log_message_list.push("succeed with clicking on button...")
+        localStorage.setItem('msg_list', JSON.stringify(log_message_list));
 
         console.log(request.message);
-        message_lst = JSON.parse(localStorage.getItem("msg_list")) 
-        for(const item_index in message_lst){
+        message_lst = JSON.parse(localStorage.getItem("msg_list"))
+        for (const item_index in message_lst) {
             log = document.createElement('p')
             log.textContent = message_lst[item_index]
             logger_box.appendChild(log)
